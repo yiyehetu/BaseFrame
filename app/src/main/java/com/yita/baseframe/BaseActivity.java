c312a11e73332eefb4048dcee355891c1b78dcb9
yiyehetu <ziruzechen@163.com>
2017-12-16 21:10:00 +0800

    引入lce布局

--- app/src/main/java/com/yita/baseframe/BaseActivity.java
+++ app/src/main/java/com/yita/baseframe/BaseActivity.java
@@ -2,7 +2,6 @@ package com.yita.baseframe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
